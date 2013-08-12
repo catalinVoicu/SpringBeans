8dafb9a7e1de85414fb6f4c09fee50a005820403
cvoicu <cvoicu@EN61089.endava.net>
2013-08-12 12:48:19 +0300

    SimpleBeanTry1

--- src/com/springAnnotations/SimpleBean.java
+++ src/com/springAnnotations/SimpleBean.java
@@ -32,6 +32,6 @@ public class SimpleBean {
 	}
 	
 	public String toString() {
-		return "Name: " + name + "\nAge " + age;s 
+		return "Name: " + name + "\nAge " + age; 
 	}
 }
--- src/com/springAnnotations/SimpleBeanMain.java
+++ src/com/springAnnotations/SimpleBeanMain.java
@@ -2,14 +2,36 @@ package com.springAnnotations;
 
 //import javax.naming.Context;
 
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 //import org.springframework.context.ApplicationContext;
 
 public class SimpleBeanMain {
 	
-	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-	ctx.load
-
+	public static void main( String args [] ){ 
+	
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:springAnnotations/simpleBean.xml");
+		ctx.refresh();
+		
+		SimpleBean sb1 = getBean("simpleBean1", ctx);
+		SimpleBean sb2 = getBean("simpleBean2", ctx);
+		SimpleBean sb3 = getBean("simpleBean3", ctx);
+		
+	}
+	
+	private static SimpleBean getBean( String beanName, ApplicationContext ctx ) {
+		try {
+			SimpleBean bean = (SimpleBean) ctx.getBean(beanName);
+			System.out.println(bean);
+			return bean;
+		} catch (BeanCreationException ex ){
+			System.out.println("An error occured in bean configuration: "
+					+ ex.getMessage());
+			return null;
+		}
+	}
 
 }
